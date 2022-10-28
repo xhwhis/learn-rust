fd8f343cf1309320156e3bdffa1858992b16ec19
xhwhis <hi@whis.me>
2022-10-28 16:10:52 +0800

    fix(rust-to-c): linking errors between Rust and C

---
+++ crates/rust-to-c/build.rs
@@ -0,0 +1,32 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    Command::new("gcc")
+        .args(&["src/assign.c", "-c", "-fPIC", "-o"])
+        .arg(PathBuf::from(&out_dir).join("assign.o"))
+        .status()
+        .unwrap();
+    Command::new("ar")
+        .args(&["crus", "libassign.a", "assign.o"])
+        .current_dir(&Path::new(&out_dir))
+        .status()
+        .unwrap();
+
+    println!("cargo:rustc-link-search=native={}", &out_dir);
+    println!("cargo:rustc-link-lib=static=assign");
+    println!("cargo:rerun-if-changed=src/assign.h");
+    let bindings = bindgen::Builder::default()
+        .header("src/assign.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file(PathBuf::from(&out_dir).join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
--- crates/rust-to-c/src/main.rs
+++ crates/rust-to-c/src/main.rs
@@ -6,6 +6,7 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 fn main() {
     let mut ab = AB { a: 1, b: 2 };
+    println!("a = {}, b = {}", ab.a, ab.b);
     unsafe {
         assign(&mut ab, 3, 4);
     }
